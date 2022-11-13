257b545063cbe9f40095e6ed329ef0e1b9517575
Yuliia <yulia.karnoza@gmail.com>
2022-11-13 17:35:47 -0500

    lab-02

--- lab-02-di/src/test/java/com/cydeo/CartServiceTest.java
+++ lab-02-di/src/test/java/com/cydeo/CartServiceTest.java
@@ -15,8 +15,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.math.BigDecimal;
 
-import static org.mockito.Mockito.when;
-
 @RunWith( SpringRunner.class )
 @ContextConfiguration(classes = ConfigApp.class)
 public class CartServiceTest {
